f38e26c52ef932f3748c31fc70ce9b28d0af49b3
Viliam Balara <vilobalara34@gmail.com>
2024-08-06 14:20:45 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,22 @@
-import { defineConfig } from 'vite'
+import restart from 'vite-plugin-restart'
 
-export default defineConfig({
-  base: "/Uaz-configurator/"
-})
+export default {
+    root: 'src/', // Sources files (typically where index.html is)
+    base: /Uaz-configurator/src,
+    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+    plugins:
+    [
+        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+    ],
+}
